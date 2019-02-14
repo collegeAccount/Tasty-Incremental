9d0fe9974c79ef77e5a767f175b1e6cddab0214e
Mommygohi <46762402+Mommygohi@users.noreply.github.com>
2019-02-13 19:51:48 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Decremental Game
+
+The game where you try to be in as much debt as possible!
---
+++ main.js
@@ -0,0 +1,56 @@
+var num_salt_water = 0;
+var num_salt = 0;
+var num_turtles = 0;
+var num_crabs = 0;
+
+function getSaltWater(input){
+    num_salt_water = num_salt_water + input;
+    document.getElementById("num_salt_water").innerHTML = "Salt Water: " + num_salt_water + "ml";
+}
+
+function dryWater(input){
+    if(num_salt_water >= input){
+      num_salt = num_salt + input;
+      num_salt_water = num_salt_water - input;
+      document.getElementById("num_salt_water").innerHTML = "Salt Water: " + num_salt_water + "ml";
+      document.getElementById("num_salt").innerHTML = "Salt: " + num_salt + "g";
+    }
+}
+
+function buyTurtle(){
+        var turtle_cost = Math.floor(10 * Math.pow(1.1,num_turtles));
+
+        if(num_salt_water >= turtle_cost){
+            num_turtles = num_turtles + 1;
+            num_salt_water = num_salt_water - turtle_cost;
+            document.getElementById('num_turtles').innerHTML = "Turtles: " + num_turtles;
+            document.getElementById('num_salt_water').innerHTML = "Salt Water: " + num_salt_water + "ml";
+            document.getElementById('salt_water_per_sec').innerHTML = "Per Sec: " + num_turtles + "ml/s"
+        }
+
+    var next_cost = Math.floor(10 * Math.pow(1.1,num_turtles));
+    document.getElementById('turtle_cost').innerHTML = "Cost: " + next_cost + "ml water";
+}
+
+function buyCrab(){
+        var crab_cost = Math.floor(10 * Math.pow(1.1,num_crabs));
+
+        if(num_salt >= crab_cost){
+            crab_cost = crab_cost + 1;
+            num_salt = num_salt - crab_cost;
+            document.getElementById('num_crabs').innerHTML = "Crabs: " + num_crabs;
+            document.getElementById('num_salt').innerHTML = "Salt: " + num_salt + "g";
+            document.getElementById('salt_per_sec').innerHTML = "Per Sec: " + num_crabs + "g/s"
+        }
+
+    var next_cost = Math.floor(10 * Math.pow(1.1,num_crabs));
+    document.getElementById('crab_cost').innerHTML = "Cost: " + next_cost + "g salt";
+}
+
+window.setInterval(function(){
+    getSaltWater(num_turtles);
+}, 1000);
+
+window.setInterval(function(){
+    dryWater(num_crabs);
+}, 1000);
