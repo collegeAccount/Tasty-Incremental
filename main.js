3e2e10ffb82434412fda2ee4418eaa942e85c355
Mommygohi <46762402+Mommygohi@users.noreply.github.com>
2019-02-19 08:30:32 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var num_salt_water = 0;
 var num_salt = 0;
 var num_turtles = 0;
 var num_crabs = 0;
+var num_birbs = 0;
 
 function getSaltWater(input){
     num_salt_water = num_salt_water + input;
@@ -44,6 +45,23 @@ function buyCrab(){
             document.getElementById('salt_per_sec').innerHTML = "Per Sec: " + num_crabs + "g/s"
             document.getElementById('salt_water_per_sec').innerHTML = "Per Sec: " + (num_turtles - num_crabs) + "ml/s"
             document.getElementById('crab_salt_per_sec').innerHTML = "Per Sec: " + num_crabs + "g/s"
+        }
+
+    var next_cost = Math.floor(10 * Math.pow(1.1,num_crabs));
+    document.getElementById('crab_cost').innerHTML = "Cost: " + next_cost + "g salt";
+}
+
+function buyBirb(){
+        var birb_cost = Math.floor(10 * Math.pow(1.1,num_birbs));
+
+        if(num_salt >= birb_cost && num_salt_water >= birb_cost){
+            num_birbs = num_birbs + 1;
+            num_salt = num_salt - birb_cost;
+            num_salt_water = num_salt_water - birb_cost;
+            document.getElementById('num_birbs').innerHTML = "Birbs: " + num_birbs;
+            document.getElementById('num_salt').innerHTML = "Salt: " + num_salt + "g";
+            document.getElementById('num_salt_water').innerHTML = "Salt Water: " + num_salt_water + "ml";
+            document.getElementById('peppercorns_per_sec').innerHTML = "Per Sec: " + num_birbs + "c/s"
         }
 
     var next_cost = Math.floor(10 * Math.pow(1.1,num_crabs));
